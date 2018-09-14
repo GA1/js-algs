03ff618a8b4a40e44f297f6b7d8d7428e2e6a949
Kamil Nguyen Van <nvkga1@gmail.com>
2018-09-14 14:55:50 +0200

    add permutationsStartingWith1

---
+++ src/permutationStartingWith.js
@@ -0,0 +1,15 @@
+
+function solve(A) {
+  const numbers = new Set()
+  const N = A.length
+  for (var i = 0; i < N; i++) {
+    var e = A[i]
+    if (N < e || numbers.has(e)) {
+      return 0
+    }
+    numbers.add(A[i])
+  }
+  return 1
+}
+
+module.exports.solve = solve;
