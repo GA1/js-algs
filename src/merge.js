0d26c22e3d976b5c397af0df6ee84e3bfe9daa60
GA1 <nvkga1@gmail.com>
2018-11-09 17:26:30 +0100

    Make recursiveMerge even more recursive

--- src/merge.js
+++ src/merge.js
@@ -3,24 +3,24 @@ function mergeRecursive(a, b) {
   function mergeRecursive(A, i, B, j, result) {
     const N = A.length;
     const M = B.length;
-    if (i === N && j === M)
-      return result
-    else {
-      if (i < N && j < M) {
-        if (A[i] <= B[j]) {
-          result.push(A[i])
-          return mergeRecursive(A, i + 1, B, j, result)
-        } else {
-          result.push(B[j])
-          return mergeRecursive(A, i, B, j + 1, result)
-        }
-      }
-      for (; i < N; i++)
+    if (i < N && j < M) {
+      if (A[i] <= B[j]) {
         result.push(A[i])
-      for (; j < M; j++)
+        return mergeRecursive(A, i + 1, B, j, result)
+      } else {
         result.push(B[j])
-      return result
+        return mergeRecursive(A, i, B, j + 1, result)
+      }
+    } else {
+      if (i < N) {
+        result.push(A[i])
+        return mergeRecursive(A, i + 1, B, j, result)
+      } else if (j < M) {
+        result.push(B[j])
+        return mergeRecursive(A, i, B, j + 1, result)
+      }
     }
+    return result
   }
   const result = []
   return mergeRecursive(a, 0, b, 0, result)
@@ -31,7 +31,7 @@ function merge(a, b) {
   const M = b.length
   const res = new Array();
   let i = 0, j = 0
-  for (; i < N && j < M;) {
+  while (i < N && j < M) {
     if (a[i] <= b[j]) {
       res.push(a[i])
       i++
