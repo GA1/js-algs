c7c9a9be4db4e21e7ab31bdada40772a55122959
GA1 <nvkga1@gmail.com>
2018-11-09 15:16:37 +0100

    add mergeRecurisve implementation

--- src/__test__/merge.test.js
+++ src/__test__/merge.test.js
@@ -2,28 +2,52 @@ var merger = require('../merge');
 
 describe('merger solver', () => {
 
-  it('merge [0] and []', () => {
-    expect(merger.merge([], [] )).toEqual([]);
+  it('merge [] and []', () => {
+    let A = [];
+    let B = [];
+    const expected = [];
+    expect(merger.mergeRecursive(A, B )).toEqual(expected);
+    expect(merger.merge(A, B )).toEqual(expected);
   })
 
   it('merge [] and [2]', () => {
-    expect(merger.merge([], [2] )).toEqual([2]);
+    const A = [];
+    const B = [2];
+    const expected = B;
+    expect(merger.mergeRecursive(A, B )).toEqual(expected);
+    expect(merger.merge(A, B )).toEqual(expected);
   })
 
   it('merge [2] and []', () => {
-    expect(merger.merge([2], [] )).toEqual([2]);
+    const A = [2];
+    const B = [];
+    const expected = [2];
+    expect(merger.mergeRecursive(A, B )).toEqual(expected);
+    expect(merger.merge(A, B )).toEqual(expected);
   })
 
   it('merge [3] and [5]', () => {
-    expect(merger.merge([3], [5] )).toEqual([3, 5]);
+    const A = [3];
+    const B = [5];
+    const expected = [3, 5];
+    expect(merger.mergeRecursive(A, B )).toEqual(expected);
+    expect(merger.merge(A, B )).toEqual(expected);
   })
 
   it('merge [5] and [3]', () => {
-    expect(merger.merge([5], [3] )).toEqual([3, 5]);
+    const A = [5];
+    const B = [3];
+    const expected = [3, 5];
+    expect(merger.mergeRecursive(A, B )).toEqual(expected);
+    expect(merger.merge(A, B )).toEqual(expected);
   })
 
   it('merge [2, 7, 11, 21] and [1, 2, 3, 4, 5]', () => {
-    expect(merger.merge([2, 7, 11, 21], [1, 2, 3, 4, 5] )).toEqual([1, 2, 2, 3, 4, 5, 7, 11, 21]);
+    const A = [2, 7, 11, 21];
+    const B = [1, 2, 3, 4, 5];
+    const expected = [1, 2, 2, 3, 4, 5, 7, 11, 21];
+    expect(merger.mergeRecursive(A, B )).toEqual(expected);
+    expect(merger.merge(A, B )).toEqual(expected);
   })
 
 })
--- src/merge.js
+++ src/merge.js
@@ -1,4 +1,31 @@
 
+function mergeRecursive(a, b) {
+  function mergeRecursive(A, i, B, j, result) {
+    const N = A.length;
+    const M = B.length;
+    if (i === N && j === M)
+      return result
+    else {
+      if (i < N && j < M) {
+        if (A[i] <= B[j]) {
+          result.push(A[i])
+          return mergeRecursive(A, i + 1, B, j, result)
+        } else {
+          result.push(B[j])
+          return mergeRecursive(A, i, B, j + 1, result)
+        }
+      }
+      for (; i < N; i++)
+        result.push(A[i])
+      for (; j < M; j++)
+        result.push(B[j])
+      return result
+    }
+  }
+  const result = []
+  return mergeRecursive(a, 0, b, 0, result)
+}
+
 function merge(a, b) {
   const N = a.length
   const M = b.length
@@ -20,4 +47,5 @@ function merge(a, b) {
   return res
 }
 
+module.exports.mergeRecursive = mergeRecursive;
 module.exports.merge = merge;
