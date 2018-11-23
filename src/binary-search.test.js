16a136e4b6057b691a835833388bb27dbd270a8b
GA1 <nvkga1@gmail.com>
2018-11-23 13:07:34 +0100

    Add binary search for array with unique items

---
+++ src/binary-search.test.js
@@ -0,0 +1,41 @@
+const solve = require('./binary-search').solve;
+
+describe('Plancks and nails', () => {
+
+  it('[0]', () => {
+    const A = [0];
+    expect(solve(A, 0)).toBe(0);
+    expect(solve(A, 2)).toBe(-1);
+  });
+
+  it('[2, 5]', () => {
+    const A = [2, 5];
+    expect(solve(A, 2)).toBe(0);
+    expect(solve(A, 5)).toBe(1);
+    expect(solve(A, 7)).toBe(-1);
+  })
+
+  it('[2, 5, 7]', () => {
+    const A = [2, 5, 7];
+    expect(solve(A, 2)).toBe(0);
+    expect(solve(A, 5)).toBe(1);
+    expect(solve(A, 7)).toBe(2);
+    expect(solve(A, 11)).toBe(-1);
+  })
+
+  it('[2, 5, 7, 11, 13, 17, 19, 23, 29, 31]', () => {
+    const A = [2, 5, 7, 11, 13, 17, 19, 23, 29];
+    expect(solve(A, 2)).toBe(0);
+    expect(solve(A, 5)).toBe(1);
+    expect(solve(A, 7)).toBe(2);
+    expect(solve(A, 11)).toBe(3);
+    expect(solve(A, 13)).toBe(4);
+    expect(solve(A, 17)).toBe(5);
+    expect(solve(A, 19)).toBe(6);
+    expect(solve(A, 23)).toBe(7);
+    expect(solve(A, 29)).toBe(8);
+    expect(solve(A, 31)).toBe(-1);
+  })
+
+
+})
