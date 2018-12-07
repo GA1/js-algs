2b530a62e4433d587109ed7026227a5589a9f5e0
GA1 <nvkga1@gmail.com>
2018-12-07 11:26:17 +0100

    Improve passing cars by reducing space complexity to O(1)

--- src/passing-cars.js
+++ src/passing-cars.js
@@ -1,24 +1,20 @@
 
 
 function solve(A) {
-  const carsGoingRightOnTheLeftFromIndex = []
-  let sum = 0;
-  for (let i = 0; i < A.length; i++) {
-	  if (A[i] === 0) {
-	    sum++
-	  }
-    carsGoingRightOnTheLeftFromIndex[i] = sum
-  }
+  let carsGoingRight = 0
   let crossings = 0
   for (let i = 0; i < A.length; i++) {
     if (A[i] === 1) {
-      crossings += carsGoingRightOnTheLeftFromIndex[i]
+      crossings += carsGoingRight
       if (1000000000 < crossings) {
         return -1
       }
+    } else {
+      carsGoingRight++
     }
   }
   return crossings
+
 }
 
 module.exports.solve = solve
