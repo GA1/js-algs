537297e6193be7cb05f2f91caf458ec59a10d779
GA1 <nvkga1@gmail.com>
2019-12-27 14:59:17 +0100

    feat: simplify key counting sorting

--- src/various/integer-sort.js
+++ src/various/integer-sort.js
@@ -2,18 +2,16 @@
 
 function integerSort(toSort) {
   const N = toSort.length;
-  const counters = Array(N).fill(0)
+  const counters = Array(N + 1).fill(0)
   for (let i = 0; i < N; i++) {
-    counters[toSort[i]]++
+    counters[toSort[i] + 1]++
   }
-  const accCounters = Array(N).fill(0)
-  accCounters[0] = 0
-  for (let i = 1; i < N; i++) {
-    accCounters[i] = accCounters[i - 1] + counters[i - 1]
+  for (let i = 0; i < N; i++) {
+    counters[i + 1] += counters[i]
   }
   const result = Array(N).fill(0)
   for (let i = 0; i < N; i++) {
-    result[accCounters[toSort[i]]++] = toSort[i]
+    result[counters[toSort[i]]++] = toSort[i]
   }
   return result
 }
