4cd10fd7051c732651ef7f4fd58ef0047267289a
GA1 <nvkga1@gmail.com>
2020-04-04 13:28:41 +0200

    feat: copy templae for the parenting partnering

---
+++ src/google-code-jam/2020/round0/3/3-solver-parenting-partnering-returns.test.js
@@ -0,0 +1,9 @@
+const { solveSingleCase } = require('./3-solver-parenting-partnering-returns')
+
+describe('2020', () => {
+  describe('23rd task: nestingDepth', () => {
+    it('0', () => {
+      expect(solveSingleCase('0')).toEqual('0')
+    })
+  })
+})
